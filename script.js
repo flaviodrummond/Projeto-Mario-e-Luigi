dcdec9560e09d8d086131c0c92716674910502de
flaviodrummond <flaviosax20@gmail.com>
2023-11-10 12:17:59 -0300

    Criando o formulário, estilizando e fazendo a chamada de envio do formulário

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let clickedButton = (clicked) => {
-    console.log('cliquei no botao')
+let form = document.querySelector('.contact-us')
+let formMask = document.querySelector('.form-mask')
+
+let clickedButton = () =>  {
+     
+    formMask.style.visibility = 'visible'
+    return form.style.left = "750px";
 }
+
+let submitForm = () => form.style.left = "-320px"

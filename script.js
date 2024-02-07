11b59cdb4bed8ef0ead80256e9a704c7e891ac26
flaviodrummond <flaviosax20@gmail.com>
2024-02-07 17:56:15 -0300

    Mudanças no formulário e no CSS

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ let formMask = document.querySelector('.form-mask')
 
 let clickedButton = () =>  {
      
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
     formMask.style.visibility = 'visible'
-    return form.style.left = "750px";
+    // return form.style.left = "750px";
 }
 
-let submitForm = () => form.style.left = "-320px"
+let hideMask = () => {
+
+    form.style.left = "-335px"
+    form.style.transform = "translateX(0)"
+    formMask.style.visibility = 'hidden'
+}
+
+
+// let submitForm = () => form.style.left = "-320px"
